ff6e86f4b690b0b141051121c6e65d843a172d90
Balaji <118836362+Balaji-Gaddam@users.noreply.github.com>
2024-01-01 18:03:56 +0530

    Code commit

---
+++ TodoLists/urls.py
@@ -0,0 +1,34 @@
+"""
+URL configuration for TodoLists project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from WebApp.views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView,RegisterView
+from django.contrib.auth.views import LogoutView
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('logout/', LogoutView.as_view(next_page = 'login'), name='logout'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+     path('register/', RegisterView.as_view(), name='register'),
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
+]
+
